efca58372f3ed4fbc3818292f4a85a59471b837c
Jeevan Reddy <cj094392@cerner.net>
2023-10-08 13:39:34 +0530

    add maven

---
+++ src/main/java/DataStructures/DataStructure/App.java
@@ -0,0 +1,13 @@
+package DataStructures.DataStructure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
